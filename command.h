50f001ed929ab24a772b61391a6baf121d8ea791
hamamah502 <85652092+hamamah502@users.noreply.github.com>
2021-06-09 20:07:19 -0400

    Update command.h

--- command.h
+++ command.h
@@ -6,21 +6,21 @@
 
 struct SubCommand
 {
-	char *line;
-	char *argv[MAX_ARGS];
+	char *line; // Sub-command stored as string
+	char *argv[MAX_ARGS]; // Sub-command broken into strings.
 };
 
 struct Command
 {
 	struct SubCommand sub_commands[MAX_SUB_COMMANDS];
 	int num_sub_commands;
-	char *stdin_redirect;
-	char *stdout_redirect;
-	int background;
+	char *stdin_redirect; // Initially NULL; indicates redirection of standard input
+	char *stdout_redirect; // Initially NULL; indicates redirection of standard output
+	int background; // Binary field; indicates whether or not command should run in background.
 };
 
 void ReadCommand(char *line, struct Command *command);
 void ReadRedirectsAndBackground(struct Command *command);
-void PrintCommand(struct Command *command);
+void PrintCommand(struct Command *command); // Test function---not used in actual shell.
 
 #endif
